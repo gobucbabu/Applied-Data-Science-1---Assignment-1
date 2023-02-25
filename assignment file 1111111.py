b328f64d767f8fbc76157911211b7611a4042955
gobucbabu <126291354+gobucbabu@users.noreply.github.com>
2023-02-25 00:47:23 +0000

    Update assignment file 1111111.py
    Plot 1 improved

--- assignment file 1111111.py
+++ assignment file 1111111.py
@@ -12,15 +12,21 @@ import matplotlib.pyplot as plt
 
 def fn1(a, b, c):
     """ Function to plot a line plot using matplotlib.pyplot """
+    plt.figure(figsize=[4.3, 2.5])
     plt.plot(a, b, label='Male', c='b')
     plt.plot(a, c, label='Female', c='g')
     plt.xlim(2000, 2019)
     plt.ylim(0, 30)
-    plt.xlabel("Year")
-    plt.ylabel("Number of deaths")
+    plt.xlabel("Year", fontweight='bold')
+    plt.ylabel("Number of deaths", fontweight='bold')
     plt.legend(loc='best')
+    plt.title("Suicide rate in india  ",
+              fontstyle='italic', color='red', fontsize=15,
+              fontweight='bold')
+    plt.savefig("Assignment 1 figure 1.png")
     plt.show()
-
+    
+#Age standardized suicide rates \n (per 100,000 population)
 
 df1 = pd.read_excel("Male and female suicide rates india.xlsx") 
 
